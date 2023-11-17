0520ea964b662bbf2dbe71afae8198372229d203
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-11-17 17:06:37 +0700

    fix: bug fix link navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="font-default w-full bg-white text-center p-3 text-2xl font-semibold">
             <h1>
-                <Link to="/wrong">
+                <Link to="/">
                     Museum<span className="text-primary">Jawa</span>
                 </Link>
             </h1>
