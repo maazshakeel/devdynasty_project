e812f65845a002ba9525b2e569fe243f56f05ec6
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-11-17 17:03:45 +0700

    fix(navbar): the whole navbar can be hovered, and now fixed so only the text can be hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className=" font-default w-full bg-white text-center p-3 text-2xl font-semibold">
-            <Link to="/">
-                <h1 className="">Museum<span className="text-primary">Jawa</span></h1>
-            </Link>
+        <nav className="font-default w-full bg-white text-center p-3 text-2xl font-semibold">
+            <h1>
+                <Link to="/wrong">
+                    Museum<span className="text-primary">Jawa</span>
+                </Link>
+            </h1>
         </nav>
     )
 }
