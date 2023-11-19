0c819cb596a2b4df71e2e75db4154fb6200906e4
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-19 21:31:16 +0700

    fix(footer.jsx): edit footer a bit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-    return(
-        <footer className="font-default font-medium p-4 bg-gray text-center w-full">
-            <p>&copy; 2023 DEVDYNASTY</p>
+    return (
+
+
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 text-center">© 2023 <Link to="https://devdynasty-project.vercel.app/" className="hover:underline">MuseumJava™</Link>. All Rights Reserved.
+                </span>
+            </div>
         </footer>
+
     )
 }
