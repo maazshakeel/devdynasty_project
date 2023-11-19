8e3f19ee7c5cdf2add331418e63fb2531271dbb0
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-19 13:13:11 +0700

    fix(homepage): resolve upside down welcome page and accordation

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,19 +1,19 @@
-import AccordionAbout from '../components/AccordionAbout'
-import Navbar from '../components/Navbar'
-import Welcome from '../components/Welcome'
+import AccordionAbout from "../components/AccordionAbout";
+import Navbar from "../components/Navbar";
+import Welcome from "../components/Welcome";
 
 function HomePage() {
   return (
     <>
-      <header className='sticky top-0'>
+      <header className="sticky top-0">
         <Navbar />
-        <AccordionAbout />
       </header>
       <main>
-        <Welcome/>
+        <Welcome />
+        <AccordionAbout />
       </main>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
