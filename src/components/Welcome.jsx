0e0f86dc4aafcdba1b92c47316778af05d1228f9
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-19 21:09:49 +0700

    fix(welcome.jsx): change image

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 export default function Welcome(){
     return(
-        <div className="w-full h-[250px] relative bg-[url(/src/assets/img/header_img.png)] bg-no-repeat bg-cover bg-center">
-            <p className="text-4xl text-white font-semibold w-1/2 z-10 font-default absolute bottom-2 left-2">Welcome To MuseumJawa!</p>
+        <div className="w-full h-[250px] relative bg-[url(/src/assets/img/header_image.png)] bg-no-repeat bg-cover bg-center">
+            <p className="lg:text-4xl sm:text-3xl md:text-4xl text-2xl opacity-90 text-white font-semibold w-1/2 z-10 font-default absolute bottom-2 left-2">Welcome To MuseumJawa!</p>
             <div className="w-full h-full absolute shadow-gradient top-0 left-0 opacity-70"></div>
         </div>
     )
