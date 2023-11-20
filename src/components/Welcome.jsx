396e44b28622710f3c1a4808a66605de699552df
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-20 09:09:09 +0700

    fix(welcome.jsx): add lazyloadimage component to render image for best practices

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,21 @@
-import LazyLoad from 'react-lazyload';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css'; // Import the blur effect CSS
+
+import Image from '/src/assets/img/header_image.png';
+import PlaceholderImage from '/src/assets/img/header_image.png'; // Replace with your actual placeholder image path
 
 export default function Welcome() {
     return (
         <div className="relative w-full h-[250px]">
-            <LazyLoad height={250} offset={100}>
-                <img
-                    src="/src/assets/img/header_image.png"
-                    alt="Header Image"
-                    className="w-full h-full object-cover object-center"
-                    style={{ maxHeight: '250px' }}
-                />
-            </LazyLoad>
+            <LazyLoadImage
+                src={Image}
+                alt="Header Image"
+                className="w-full h-full object-cover object-center"
+                style={{ maxHeight: '250px', width: '100%', height: '250px' }}
+                width={'100%'} // Set your desired width
+                placeholderSrc={PlaceholderImage}
+                effect="blur"
+            />
             <p className="absolute bottom-2 left-2 z-10 w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-4xl opacity-90 text-white font-semibold font-default">
                 Welcome To MuseumJawa!
             </p>
