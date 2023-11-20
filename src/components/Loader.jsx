8d96bb33faea468a7597fa302a927c2ee4851a76
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-20 08:49:54 +0700

    fix(loader): center loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,8 @@
 const Loader = () => {
-    let circleCommonClasses = 'h-14 w-14 bg-green-600  rounded-full';
+    let circleCommonClasses = 'h-14 w-14 bg-green-600 rounded-full';
 
     return (
-        <div className='flex mx-auto gap-5'>
+        <div className='flex justify-center items-center h-screen gap-5'>
             <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
             <div
                 className={`${circleCommonClasses} mr-1 animate-bounce200`}
