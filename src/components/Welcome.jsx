75ab7a971ae18b49e16171658757a10897074471
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-20 08:59:29 +0700

    feat(welcome.jsx): use lazyload to render image

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,16 @@
+import LazyLoad from 'react-lazyload';
+
 export default function Welcome() {
     return (
-        <div className="relative w-full h-[250px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(/src/assets/img/header_image.png)' }}>
+        <div className="relative w-full h-[250px]">
+            <LazyLoad height={250} offset={100}>
+                <img
+                    src="/src/assets/img/header_image.png"
+                    alt="Header Image"
+                    className="w-full h-full object-cover object-center"
+                    style={{ maxHeight: '250px' }}
+                />
+            </LazyLoad>
             <p className="absolute bottom-2 left-2 z-10 w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-4xl opacity-90 text-white font-semibold font-default">
                 Welcome To MuseumJawa!
             </p>
