7b0494c47b7587f1d01600c876246328a852e71c
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-20 08:41:07 +0700

    fix(welcome.jsx): optimized a bit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,10 @@
-export default function Welcome(){
-    return(
-        <div className="w-full h-[250px] relative bg-[url(/src/assets/img/header_image.png)] bg-no-repeat bg-cover bg-center">
-            <p className="lg:text-4xl sm:text-3xl md:text-4xl text-2xl opacity-90 text-white font-semibold w-1/2 z-10 font-default absolute bottom-2 left-2">Welcome To MuseumJawa!</p>
-            <div className="w-full h-full absolute shadow-gradient top-0 left-0 opacity-70"></div>
+export default function Welcome() {
+    return (
+        <div className="relative w-full h-[250px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(/src/assets/img/header_image.png)' }}>
+            <p className="absolute bottom-2 left-2 z-10 w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-4xl opacity-90 text-white font-semibold font-default">
+                Welcome To MuseumJawa!
+            </p>
+            <div className="absolute top-0 left-0 w-full h-full opacity-70 shadow-gradient"></div>
         </div>
-    )
+    );
 }
