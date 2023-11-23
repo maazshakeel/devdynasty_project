3b0b89ec5671219cea8b951aeb95f291ec0b9e49
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-23 10:24:14 +0700

    refactor(welcome): fixing

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -12,8 +12,9 @@ function HomePage() {
     "https://source.unsplash.com/755x480?mountain",
     "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
   ];
+
   return (
-    <>
+    <div>
       <header className="sticky top-0 z-50">
         <Navbar />
       </header>
@@ -25,17 +26,17 @@ function HomePage() {
             Lebih Lanjut!</p>
         </div>
         <div className="w-full sm:w-[75%] mt-5 sm:mx-auto md:w-[60%]">
-        <h2 className="text-3xl font-semibold font-default ml-5 sm:ml-0">Preview</h2>
-        <div className="pt-7">
-          <Slider slides={slides} />
-        </div>
+          <h2 className="text-3xl font-semibold font-default ml-5 sm:ml-0">Preview</h2>
+          <div className="pt-7">
+            <Slider slides={slides} />
+          </div>
         </div>
         <AccordionAbout />
       </main>
       <footer>
         <Footer />
       </footer>
-    </>
+    </div>
   );
 }
 
