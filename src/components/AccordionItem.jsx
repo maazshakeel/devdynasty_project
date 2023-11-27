6cf9b127b888c967f4e9f93f39c49c8f600ee0a1
maazshakeel <maaz.shakeel.bodla@gmail.com>
2023-11-27 18:28:35 +0700

    feat(style): :art: Change cursor

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -2,43 +2,53 @@ import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 export default function AccordionItem({ title, content }) {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    function toggleClick() {
-        setIsOpen(!isOpen);
-    }
+  function toggleClick() {
+    setIsOpen(!isOpen);
+  }
 
-    // Generate a unique ID for the heading
-    const headingId = `accordion-heading-${title.replace(/\s+/g, "-").toLowerCase()}`;
+  // Generate a unique ID for the heading
+  const headingId = `accordion-heading-${title
+    .replace(/\s+/g, "-")
+    .toLowerCase()}`;
 
-    return (
-        <div onClick={toggleClick} className="border border-[#B6C2E2] rounded-lg hover:cursor-pointer px-5 py-2 font-default sm:w-[75%] sm:self-center md:w-[60%]">
-            <div className="flex justify-between">
-                <h2 id={headingId} className="font-semibold text-lg md:text-xl">
-                    {title}
-                </h2>
-                <button aria-labelledby={headingId} onClick={toggleClick}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="12"
-                        height="7"
-                        viewBox="0 0 12 7"
-                        fill="none"
-                        style={{
-                            transform: isOpen ? "rotate(-180deg)" : "rotate(0deg)",
-                            transition: "0.2s ease-out",
-                        }}
-                    >
-                        <path d="M2.12 0.290001L6 4.17L9.88 0.290001C10.27 -0.0999986 10.9 -0.0999986 11.29 0.290001C11.68 0.680001 11.68 1.31 11.29 1.7L6.7 6.29C6.31 6.68 5.68 6.68 5.29 6.29L0.700001 1.7C0.310001 1.31 0.310001 0.680001 0.700001 0.290001C1.09 -0.0899986 1.73 -0.0999986 2.12 0.290001Z" fill="#323232" />
-                    </svg>
-                </button>
-            </div>
-            <div
-                className={`overflow-hidden transition ${isOpen ? "max-h-96" : "max-h-0 "}`}
-                style={{ transition: isOpen ? "0.5s " : "0.1s " }}
-            >
-                <p>{content}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      onClick={toggleClick}
+      className="border border-[#B6C2E2] rounded-lg px-5 py-2 font-default sm:w-[75%] sm:self-center md:w-[60%]"
+    >
+      <div className="flex justify-between">
+        <h2 id={headingId} className="acc font-semibold text-lg md:text-xl">
+          {title}
+        </h2>
+        <button aria-labelledby={headingId} onClick={toggleClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="7"
+            viewBox="0 0 12 7"
+            fill="none"
+            style={{
+              transform: isOpen ? "rotate(-180deg)" : "rotate(0deg)",
+              transition: "0.2s ease-out",
+            }}
+          >
+            <path
+              d="M2.12 0.290001L6 4.17L9.88 0.290001C10.27 -0.0999986 10.9 -0.0999986 11.29 0.290001C11.68 0.680001 11.68 1.31 11.29 1.7L6.7 6.29C6.31 6.68 5.68 6.68 5.29 6.29L0.700001 1.7C0.310001 1.31 0.310001 0.680001 0.700001 0.290001C1.09 -0.0899986 1.73 -0.0999986 2.12 0.290001Z"
+              fill="#323232"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`overflow-hidden transition ${
+          isOpen ? "max-h-96" : "max-h-0 "
+        }`}
+        style={{ transition: isOpen ? "0.5s " : "0.1s " }}
+      >
+        <p>{content}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,52 +3,59 @@ import { FaChevronRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 
 export default function Slider({ slides }) {
-    const [current, setCurrent] = useState(0);
+  const [current, setCurrent] = useState(0);
 
-    const previousSlide = () => {
-        setCurrent((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
-    };
+  const previousSlide = () => {
+    setCurrent((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+  };
 
-    const nextSlide = () => {
-        setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
-    };
+  const nextSlide = () => {
+    setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  };
 
-    return (
-        <div className="overflow-hidden relative rounded-md">
-            <div
-                className={`flex transition ease-out duration-400`}
-                style={{
-                    transform: `translateX(-${current * 100}%)`,
-                }}
-            >
-                {slides.map((s, index) => (
-                    <img key={index} src={s} alt={`Slide ${index + 1}`} width={'auto'} height={'auto'}/>
-                ))}
-            </div>
+  return (
+    <div className="overflow-hidden relative rounded-md">
+      <div
+        className={`flex transition ease-out duration-400`}
+        style={{
+          transform: `translateX(-${current * 100}%)`,
+        }}
+      >
+        {slides.map((s, index) => (
+          <img
+            key={index}
+            src={s}
+            alt={`Slide ${index + 1}`}
+            width={"auto"}
+            height={"auto"}
+          />
+        ))}
+      </div>
 
-            <div className="absolute bottom-3 h-full w-full justify-between items-center flex text-white text-3xl px-4 sm:px-6 ">
-                <button onClick={previousSlide} aria-label="Previous Slide">
-                    <FaChevronLeft />
-                </button>
-                <button onClick={nextSlide} aria-label="Next Slide">
-                    <FaChevronRight />
-                </button>
-            </div>
+      <div className="absolute bottom-3 h-full w-full justify-between items-center flex text-white text-3xl px-4 sm:px-6 ">
+        <button onClick={previousSlide} aria-label="Previous Slide">
+          <FaChevronLeft />
+        </button>
+        <button onClick={nextSlide} aria-label="Next Slide">
+          <FaChevronRight />
+        </button>
+      </div>
 
-            <div className="absolute bottom-0 py-3 lg:py-6 flex justify-center gap-2 lg:gap-3 w-full">
-                {slides.map((s, i) => (
-                    <div
-                        onClick={() => {
-                            setCurrent(i);
-                        }}
-                        key={"circle" + i}
-                        className={`rounded-full w-1 h-1 lg:w-2 lg:h-2 cursor-pointer  ${i === current ? "bg-white" : "bg-slate-500"
-                            }`}
-                        role="button"
-                        aria-label={`Go to Slide ${i + 1}`}
-                    ></div>
-                ))}
-            </div>
-        </div>
-    );
+      <div className="absolute bottom-0 py-3 lg:py-6 flex justify-center gap-2 lg:gap-3 w-full">
+        {slides.map((s, i) => (
+          <div
+            onClick={() => {
+              setCurrent(i);
+            }}
+            key={"circle" + i}
+            className={`rounded-full w-1 h-1 lg:w-2 lg:h-2 cursor-pointer  ${
+              i === current ? "bg-white" : "bg-slate-500"
+            }`}
+            role="button"
+            aria-label={`Go to Slide ${i + 1}`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
