83f37c491acbafe4b500a849aea55ca99680ddec
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-05 14:30:02 +0700

    feat: add logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import { AppContextProvider } from "../context/AppContext";
-import { useContext } from "react";
 import { useAppContext } from "../context/AppContext";
 
 export default function Navbar() {
@@ -12,11 +10,19 @@ export default function Navbar() {
         borderBottomColor: isOpen && "black",
         borderBottomWidth: isOpen && "1px",
       }}
-      className={`font-default w-full bg-[#E8F0C1] text-center py-3 pb-5 text-2xl font-semibold`}
+      className={`font-default w-full bg-[#E8F0C1] text-center py-3 pb-3 mb-3 text-2xl font-semibold border-solid border-b-2`}
     >
       <h1>
-        <Link to="/">
-          Museum<span className="text-primary">Jawa</span>
+        <Link to="/" className="flex items-center justify-center">
+          <img
+            src="/img/logo.png"
+            width={22}
+            height={22}
+            className="mr-1 pb-1"
+          />
+          <div>
+            Museum<span className="text-primary">Jawa</span>
+          </div>
         </Link>
       </h1>
     </nav>
